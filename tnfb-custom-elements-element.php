ba621fc319a07362a39d65d017a6112d7e8c8038
emontzka <emontzka@jnlcom.com>
2025-08-14 11:47:16 -0500

    comment out statements link

--- tnfb-custom-elements-element.php
+++ tnfb-custom-elements-element.php
@@ -212,7 +212,7 @@ if ( ! class_exists( 'tnfblatestnews' ) ) {
 		$html .= '<li><a href="#" data-category="ag-news">Ag News</a></li>';
 		$html .= '<li><a href="#" data-category="ag-angle">Ag Angle</a></li>';
 		$html .= '<li><a href="#" data-category="press-releases">Press Releases</a></li>';
-		$html .= '<li><a href="#" data-category="statements">Statements</a></li>';
+		// $html .= '<li><a href="#" data-category="statements">Statements</a></li>';
 		$html .= '</ul></div>';
 		if ($news_query->have_posts()):
 			$html .= '<div class="vc_row latest-news">';
